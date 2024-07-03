03475d4550bfebe414239aa85e9057e533e4617c
Souparna Bose <souparnabose99@gmail.com>
2024-07-03 20:18:55 +0530

    Update ApiConnect

---
+++ ApiConnect/src/main/java/com/api/ApiConnect/ApiConnectApplication.java
@@ -0,0 +1,13 @@
+package com.api.ApiConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConnectApplication.class, args);
+	}
+
+}
