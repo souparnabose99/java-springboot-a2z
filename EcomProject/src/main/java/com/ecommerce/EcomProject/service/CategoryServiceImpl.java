40f3a74c3936951c4b76472e5b59b822e5d2b042
Souparna Bose <souparnabose99@gmail.com>
2024-07-08 12:33:08 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/service/CategoryServiceImpl.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/service/CategoryServiceImpl.java
@@ -1,10 +1,12 @@
 package com.ecommerce.EcomProject.service;
 
 import com.ecommerce.EcomProject.model.Category;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CategoryServiceImpl implements CategoryService{
 
     private List<Category> categories = new ArrayList<>();
