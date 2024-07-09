e7996113e5ea1c0703450a720c3929d7a2504fdc
Souparna Bose <souparnabose99@gmail.com>
2024-07-09 12:33:45 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/service/CategoryServiceImpl.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/service/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ecommerce.EcomProject.service;
 
 import com.ecommerce.EcomProject.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,10 +28,9 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().orElse(null);
-
-        if (category == null)
-            return "Category with categoryId: " + categoryId + " not found !!";
+        Category category = categories.stream().
+                filter(c -> c.getCategoryId().equals(categoryId)).findFirst().
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
         categories.remove(category);
         return "Category with categoryId: " + categoryId + " has been deleted !!";
