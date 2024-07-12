377c896e29c388fae285674496345205220d9f7d
Souparna Bose <souparnabose99@gmail.com>
2024-07-12 14:29:49 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/model/Category.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/model/Category.java
@@ -1,12 +1,15 @@
 package com.ecommerce.EcomProject.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "CATEGORIES")
 public class Category {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
     private String categoryName;
