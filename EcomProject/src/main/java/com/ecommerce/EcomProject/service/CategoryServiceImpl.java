c308bf48f73a107616e4200b7032976a86a067db
Souparna Bose <souparnabose99@gmail.com>
2024-07-13 17:21:35 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/service/CategoryServiceImpl.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecommerce.EcomProject.service;
 
 import com.ecommerce.EcomProject.model.Category;
+import com.ecommerce.EcomProject.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,43 +14,58 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImpl implements CategoryService{
 
-    private List<Category> categories = new ArrayList<>();
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    //private List<Category> categories = new ArrayList<>();
 
     private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories(){
-        return categories;
+        //return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category){
         category.setCategoryId(nextId++);
-        categories.add(category);
+        //categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category = categories.stream().
-                filter(c -> c.getCategoryId().equals(categoryId)).findFirst().
-                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+        Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+        //List<Category> categories = categoryRepository.findAll();
+        //Category category = categories.stream().
+        //        filter(c -> c.getCategoryId().equals(categoryId)).findFirst().
+        //        orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
-        categories.remove(category);
+        //categories.remove(category);
+        categoryRepository.delete(category);
         return "Category with categoryId: " + categoryId + " has been deleted !!";
     }
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        Optional<Category> optionalCategory = categories.stream().
-                filter(c -> c.getCategoryId().equals(categoryId)).findFirst();
+        //List<Category> categories = categoryRepository.findAll();
+        //Optional<Category> savedCategoryOptional = categoryRepository.findById(categoryId);
+        //Optional<Category> optionalCategory = categories.stream().
+        //        filter(c -> c.getCategoryId().equals(categoryId)).findFirst();
 
-        if (optionalCategory.isPresent()){
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            return existingCategory;
-        } else{
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-        }
+        //if (optionalCategory.isPresent()){
+        //    Category existingCategory = optionalCategory.get();
+        //    existingCategory.setCategoryName(category.getCategoryName());
+        //    return categoryRepository.save(existingCategory);
+        // } else{
+        //            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
+        //        }
+        Category savedCategory =  categoryRepository.findById(categoryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+        category.setCategoryId(categoryId);
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
+
     }
 
 }
