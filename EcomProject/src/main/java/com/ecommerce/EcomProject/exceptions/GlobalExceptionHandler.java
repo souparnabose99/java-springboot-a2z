3371a96daf16dd3fdaa0a296f4d91304d321150f
Souparna Bose <souparnabose99@gmail.com>
2024-07-14 11:57:53 +0530

    Update EcomProject

---
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.ecommerce.EcomProject.exceptions;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@ExceptionHandler
+public class GlobalExceptionHandler {
+}
