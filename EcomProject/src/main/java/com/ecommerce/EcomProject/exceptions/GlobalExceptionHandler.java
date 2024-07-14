b482767151e785f091bee69d7b817be583bc9eeb
Souparna Bose <souparnabose99@gmail.com>
2024-07-14 12:01:41 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/exceptions/GlobalExceptionHandler.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/exceptions/GlobalExceptionHandler.java
@@ -3,7 +3,13 @@ package com.ecommerce.EcomProject.exceptions;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Map;
+
 @RestControllerAdvice
 @ExceptionHandler
 public class GlobalExceptionHandler {
+
+    public Map<String, String> myMethodArgumentNotValidException(){
+        
+    }
 }
