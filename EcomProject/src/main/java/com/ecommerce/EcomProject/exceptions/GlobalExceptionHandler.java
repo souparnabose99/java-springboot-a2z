5b3e7e13ea7f6065c26853f755fba713f95b428b
Souparna Bose <souparnabose99@gmail.com>
2024-07-15 12:12:54 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/exceptions/GlobalExceptionHandler.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,24 @@
 package com.ecommerce.EcomProject.exceptions;
 
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-@ExceptionHandler
 public class GlobalExceptionHandler {
 
-    public Map<String, String> myMethodArgumentNotValidException(){
-        
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> myMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String, String> response = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(err-> {
+            String fieldName = ((FieldError)(err)).getField();
+            String message = err.getDefaultMessage();
+            response.put(fieldName, message);
+        });
+        return response;
     }
 }
