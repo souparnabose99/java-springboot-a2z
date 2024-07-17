264a7619d317993ecaf2ec6c446eb9a254889851
Souparna Bose <souparnabose99@gmail.com>
2024-07-17 11:39:12 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/controller/CategoryController.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/controller/CategoryController.java
@@ -40,22 +40,22 @@ public class CategoryController {
 
     @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
-        try {
-            String status = categoryService.deleteCategory(categoryId);
-            return new ResponseEntity<>(status, HttpStatus.OK);
-        } catch (ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+        //try {
+        String status = categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(status, HttpStatus.OK);
+        //} catch (ResponseStatusException e){
+        //    return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        //}
     }
 
     @PutMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId){
-        try{
-            Category savedCategory = categoryService.updateCategory(category, categoryId);
-            return new ResponseEntity<>("Category with categoryId: " + categoryId + " updated !!", HttpStatus.OK);
-        } catch(ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+    public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category, @PathVariable Long categoryId){
+        //try{
+        Category savedCategory = categoryService.updateCategory(category, categoryId);
+        return new ResponseEntity<>("Category with categoryId: " + categoryId + " updated !!", HttpStatus.OK);
+        //} catch(ResponseStatusException e){
+        //    return new ResponseEntity<>(e.getReason(), e.getStatusCode());
+        //}
     }
 
 }
