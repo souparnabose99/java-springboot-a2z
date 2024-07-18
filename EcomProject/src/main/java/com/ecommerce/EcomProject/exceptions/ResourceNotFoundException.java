9ce71a24f65c3e6f87012868d6049568507e7d4b
Souparna Bose <souparnabose99@gmail.com>
2024-07-18 21:48:18 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/exceptions/ResourceNotFoundException.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,25 @@
 package com.ecommerce.EcomProject.exceptions;
 
-public class ResourceNotFoundException {
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ResourceNotFoundException extends RuntimeException{
+    String resourceName;
+    String fieldName;
+    String field;
+    Long fieldId;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, String field) {
+        super(String.format("%s not found with %s: %s", field, resourceName, fieldName));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.field = field;
+    }
+
+    public ResourceNotFoundException(String field, String resourceName, Long fieldId) {
+        super(String.format("%s not found with %s: %d", field, resourceName, fieldId));
+        this.field = field;
+        this.resourceName = resourceName;
+        this.fieldId = fieldId;
+    }
 }
