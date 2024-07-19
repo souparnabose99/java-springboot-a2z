4d2eb9d8d1cc5bdf3eee41b73126f16e9019caab
Souparna Bose <souparnabose99@gmail.com>
2024-07-19 18:17:48 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/service/CategoryService.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/service/CategoryService.java
@@ -1,12 +1,16 @@
 package com.ecommerce.EcomProject.service;
 
 import com.ecommerce.EcomProject.model.Category;
+import com.ecommerce.EcomProject.payload.CategoryDTO;
+import com.ecommerce.EcomProject.payload.CategoryResponse;
 
 import java.util.List;
 
 public interface CategoryService {
-    List<Category> getAllCategories();
-    void createCategory(Category category);
+    //List<Category> getAllCategories();
+    CategoryResponse getAllCategories();
+    //void createCategory(Category category);
+    CategoryDTO createCategory(CategoryDTO categoryDTO);
     String deleteCategory(Long categoryId);
     Category updateCategory(Category category, Long categoryId);
 }
