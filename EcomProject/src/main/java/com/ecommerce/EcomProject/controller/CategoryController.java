abf2efcc6af501b7118a0361e57acd04363d8a4a
Souparna Bose <souparnabose99@gmail.com>
2024-07-19 18:17:08 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/controller/CategoryController.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.EcomProject.controller;
 
 import com.ecommerce.EcomProject.model.Category;
+import com.ecommerce.EcomProject.payload.CategoryDTO;
+import com.ecommerce.EcomProject.payload.CategoryResponse;
 import com.ecommerce.EcomProject.service.CategoryService;
 import com.ecommerce.EcomProject.service.CategoryServiceImpl;
 import jakarta.validation.Valid;
@@ -27,15 +29,20 @@ public class CategoryController {
     */
 
     @GetMapping("/public/categories")
-    public ResponseEntity<List<Category>> getAllCategories(){
-        List<Category> categories = categoryService.getAllCategories();
-        return new ResponseEntity<>(categories, HttpStatus.OK);
+    //public ResponseEntity<List<Category>> getAllCategories(){
+    public ResponseEntity<CategoryResponse> getAllCategories(){
+        CategoryResponse categoryResponse = categoryService.getAllCategories();
+        //return new ResponseEntity<>(categories, HttpStatus.OK);
+        return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
     @PostMapping("/admin/categories")
-    public ResponseEntity<String> createCategory(@Valid @RequestBody Category category){
-        categoryService.createCategory(category);
-        return new ResponseEntity<>("Category added successfully !!", HttpStatus.CREATED);
+    //public ResponseEntity<String> createCategory(@Valid @RequestBody Category category){
+    public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO){
+        // categoryService.createCategory(category);
+        CategoryDTO savedCategoryDTO = categoryService.createCategory(categoryDTO);
+        //return new ResponseEntity<>("Category added successfully !!", HttpStatus.CREATED);
+        return new ResponseEntity<>(savedCategoryDTO, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/admin/categories/{categoryId}")
