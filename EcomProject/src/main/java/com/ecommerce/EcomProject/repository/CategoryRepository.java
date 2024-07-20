cebf2fa2832448809b65ba9b17ebf95a5f14e0b6
Souparna Bose <souparnabose99@gmail.com>
2024-07-20 22:37:51 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/repository/CategoryRepository.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/repository/CategoryRepository.java
@@ -4,4 +4,6 @@ import com.ecommerce.EcomProject.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Category findByCategoryName(String categoryName);
 }
