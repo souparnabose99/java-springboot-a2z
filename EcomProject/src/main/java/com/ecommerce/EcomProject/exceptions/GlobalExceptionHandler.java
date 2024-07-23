32fc13c2ccfa9912a98db3af6284bea68f48c90b
Souparna Bose <souparnabose99@gmail.com>
2024-07-23 19:32:59 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/exceptions/GlobalExceptionHandler.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ecommerce.EcomProject.exceptions;
 
+import com.ecommerce.EcomProject.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -25,15 +26,17 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e){
+    public ResponseEntity<APIResponse> myResourceNotFoundException(ResourceNotFoundException e){
         String message = e.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        APIResponse apiResponse = new APIResponse(message, false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<String> myApiException(ApiException e){
+    public ResponseEntity<APIResponse> myApiException(ApiException e){
         String message = e.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        APIResponse apiResponse = new APIResponse(message, false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
