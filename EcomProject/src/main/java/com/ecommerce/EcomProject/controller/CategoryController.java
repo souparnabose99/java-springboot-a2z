8d696e059cb194e8c1772093964d12d8f45d1146
Souparna Bose <souparnabose99@gmail.com>
2024-07-23 17:12:44 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/controller/CategoryController.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/controller/CategoryController.java
@@ -30,10 +30,17 @@ public class CategoryController {
 
     @GetMapping("/public/categories")
     //public ResponseEntity<List<Category>> getAllCategories(){
-    public ResponseEntity<CategoryResponse> getAllCategories(){
-        CategoryResponse categoryResponse = categoryService.getAllCategories();
+    public ResponseEntity<CategoryResponse> getAllCategories(
+            @RequestParam(name = "pageNumber") Integer pageNumber,
+            @RequestParam(name = "pageSize") Integer pageSize){
+        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
         //return new ResponseEntity<>(categories, HttpStatus.OK);
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/echo")
+    public ResponseEntity<String> echoMessage(@RequestParam(name = "message", required = false) String message){
+        return new ResponseEntity<>("Echoed message : " + message, HttpStatus.OK);
     }
 
     @PostMapping("/admin/categories")
