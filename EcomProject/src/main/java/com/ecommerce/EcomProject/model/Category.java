aa3e6365cd47ac9d7b5c1b5d45cb9e9fc0f81231
Souparna Bose <souparnabose99@gmail.com>
2024-07-28 10:28:44 +0530

    Update EcomProject

--- EcomProject/src/main/java/com/ecommerce/EcomProject/model/Category.java
+++ EcomProject/src/main/java/com/ecommerce/EcomProject/model/Category.java
@@ -24,30 +24,4 @@ public class Category {
     @Size(min = 5, message = "Category name must contain atleast 5 characters")
     private String categoryName;
 
-    /*
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Category(){
-
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-     */
 }
