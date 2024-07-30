d6a945ea50d877bdb2d5e993696f931f3a555e63
Souparna Bose <souparnabose99@gmail.com>
2024-07-30 13:42:02 +0530

    Update SpringSecurity project

--- SpringSecurity/src/main/java/com/security/SpringSecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +18,7 @@ public class SecurityConfig {
             ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
         });
         //http.formLogin(Customizer.withDefaults());
+        http.sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
     }
