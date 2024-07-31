da7aa0b65cb0f5004800171342b26c622f8fe208
Souparna Bose <souparnabose99@gmail.com>
2024-07-31 13:51:15 +0530

    Update SpringSecurity project

--- SpringSecurity/src/main/java/com/security/SpringSecurity/controller/SpringSecurityController.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/controller/SpringSecurityController.java
@@ -1,7 +1,9 @@
 package com.security.SpringSecurity.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class SpringSecurityController {
 
     @GetMapping("/security")
