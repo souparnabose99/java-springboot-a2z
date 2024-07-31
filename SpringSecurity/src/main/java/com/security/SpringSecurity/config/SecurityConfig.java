adc614095ec0668eb301c90a88e4b4ab08caa802
Souparna Bose <souparnabose99@gmail.com>
2024-07-31 13:51:52 +0530

    Update SpringSecurity project

--- SpringSecurity/src/main/java/com/security/SpringSecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/config/SecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +25,12 @@ public class SecurityConfig {
         http.sessionManagement(session ->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user").
+                password("{noop}password").roles("USER").build();
+        return new InMemoryUserDetailsManager(user1);
     }
 }
