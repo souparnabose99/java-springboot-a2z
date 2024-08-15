b2621e0aeead5de6fd0dbe27ec7956b6d6e2a839
Souparna Bose <souparnabose99@gmail.com>
2024-08-15 13:12:53 +0530

    Update SpringSecurity

--- SpringSecurity/src/main/java/com/security/SpringSecurity/controller/SpringSecurityController.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/controller/SpringSecurityController.java
@@ -1,5 +1,6 @@
 package com.security.SpringSecurity.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ public class SpringSecurityController {
         return "Spring security demo !!";
     }
 
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/user")
     public String userEndpoint(){
         return "Hello User !!";
