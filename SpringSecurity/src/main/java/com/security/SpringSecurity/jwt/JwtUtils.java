5719e3956fcd5a8723807ae6fe24c84eb095f106
Souparna Bose <souparnabose99@gmail.com>
2024-08-22 10:00:32 +0530

    Update SpringSecurity

--- SpringSecurity/src/main/java/com/security/SpringSecurity/jwt/JwtUtils.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/jwt/JwtUtils.java
@@ -1,4 +1,27 @@
 package com.security.SpringSecurity.jwt;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${spring.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${spring.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String getJwtFromHeader(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        logger.debug("Authorization Header: {}", bearerToken);
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 }
