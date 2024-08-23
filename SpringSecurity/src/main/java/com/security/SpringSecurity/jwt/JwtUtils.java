85e5f635d2f4436844d13e4adce5a6e34f3bd8c3
Souparna Bose <souparnabose99@gmail.com>
2024-08-23 10:10:01 +0530

    Update SpringSecurity

--- SpringSecurity/src/main/java/com/security/SpringSecurity/jwt/JwtUtils.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/jwt/JwtUtils.java
@@ -1,10 +1,19 @@
 package com.security.SpringSecurity.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+import static jdk.internal.org.jline.keymap.KeyMap.key;
 
 @Component
 public class JwtUtils {
@@ -23,5 +32,19 @@ public class JwtUtils {
             return bearerToken.substring(7);
         }
         return null;
+    }
+
+    public String generateTokenFromUsername(UserDetails userDetails){
+        String username = userDetails.getUsername();
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key())
+                .compact();
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 }
