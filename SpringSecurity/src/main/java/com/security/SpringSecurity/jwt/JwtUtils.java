4b07ed9aa20b47419d245d843d749fdad913fe49
Souparna Bose <souparnabose99@gmail.com>
2024-08-24 10:20:45 +0530

    Update SpringSecurity

--- SpringSecurity/src/main/java/com/security/SpringSecurity/jwt/JwtUtils.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/jwt/JwtUtils.java
@@ -1,6 +1,9 @@
 package com.security.SpringSecurity.jwt;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
@@ -44,7 +48,36 @@ public class JwtUtils {
                 .compact();
     }
 
+    public String getUserNameFromJwtToken(String token){
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
     private Key key(){
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public Boolean validateJwtToken(String authToken){
+        try{
+            System.out.println("Validate");
+            Jwts.parser()
+                    .verifyWith((SecretKey) key())
+                    .build()
+                    .parseSignedClaims(authToken);
+            return true;
+        } catch (MalformedJwtException e){
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e){
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e){
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e){
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
     }
 }
